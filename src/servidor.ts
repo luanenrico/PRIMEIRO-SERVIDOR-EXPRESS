6f1b3febe65a4c9e2c2398290e79dcbffbcc3277
LUAN <luan@MacBook-Pro-de-LUAN.local>
2024-08-16 15:59:04 -0300

    Servidor configurado com typescript, express  e javascript

--- src/servidor.ts
+++ src/servidor.ts
@@ -1,8 +1,8 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const servidor = express()
 
-servidor.get("/", (requisicao, resposta) => {
+servidor.get("/", (requisicao: Request, resposta:Response) => {
     return resposta.send("Olá dev, seja bem vindo! Este é o meu primeiro servidor com Node.js, com express e typescript")
 })
 
