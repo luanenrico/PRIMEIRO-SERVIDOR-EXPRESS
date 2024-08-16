8f08cf60811305584f361c144607b22d4c8bdf77
LUAN <luan@MacBook-Pro-de-LUAN.local>
2024-08-16 15:28:52 -0300

    corrigido erros de configuração

--- src/servidor.js
+++ src/servidor.js
@@ -1,10 +1,9 @@
-import express from express
+import express from "express"
 
 const servidor = express()
 
 servidor.get("/", (requisicao, resposta) => {
-    const saudacao = "Olá dev, seja bem vindo! Este é o meu primeiro servidor com Node.js, com express e typescript"
-    return resposta.send(saudacao)
+    return resposta.send("Olá dev, seja bem vindo! Este é o meu primeiro servidor com Node.js, com express e typescript")
 })
 
 servidor.listen(3000)
